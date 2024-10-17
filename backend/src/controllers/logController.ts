b836b4ee258c9da8e1228924d1080d97b832b5dd
DouglasBSalvador <douglasborota@hotmail.com>
2024-10-16 23:26:44 -0300

    entrega07(back): faz back se integrar com API do ChatGPT

--- backend/src/controllers/logController.ts
+++ backend/src/controllers/logController.ts
@@ -1,6 +1,10 @@
+import { OpenAI } from 'openai'
 import { Request, Response } from 'express'
 import db from '../config/prisma'
 
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
 
 export const consultAndLog = async (req: Request, res: Response) => {
   const question = req.query.question as string
@@ -13,10 +17,20 @@ export const consultAndLog = async (req: Request, res: Response) => {
   const decodedQuestion = decodeURIComponent(question)
 
   try {
-    const answer = 'Teste'
+    // Requisição à API do ChatGPT
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: decodedQuestion }, {role: 'system', content: "You are a videogame expert."}],
+      temperature: 0.7,
+      max_tokens: 200,
+      top_p: 1,
+      stop: ['\n']
+    })
+
+    const answer = response.choices[0]?.message?.content
 
     if (!answer) {
-      return res.json({ error: 'Resposta inválida da API' })
+      return res.json({ error: 'Resposta inválida da API do ChatGPT' })
     }
 
     // Salvar log no banco de dados
@@ -30,9 +44,9 @@ export const consultAndLog = async (req: Request, res: Response) => {
     // Retornar o log criado e a resposta da API
     return res.json({ log: newLog, answer })
   } catch (error: any) {
-    console.error('Erro ao consultar API', error)
+    console.error('Erro ao consultar API do ChatGPT:', error)
     return res.json({
-      error: 'Erro ao consultar API',
+      error: 'Erro ao consultar API do ChatGPT',
       details: error.message,
     })
   }
