96ba8b258e10b55215c395f024652be71ef3d4cc
DouglasBSalvador <douglasborota@hotmail.com>
2024-10-16 23:14:02 -0300

    entrega05(back): cria a tabela de logs

---
+++ backend/prisma/migrations/20241017021228_init_db/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Log" (
+    "id" SERIAL NOT NULL,
+    "question" TEXT NOT NULL,
+    "answer" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
+);
